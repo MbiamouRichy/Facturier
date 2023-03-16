7795a665b19505e682da7430f4fadb28e714fbef
Richy <richymbiamou52@gmail.com>
2023-03-16 23:13:50 +0100

    Partie Exercice facturier 2

--- src/script.ts
+++ src/script.ts
@@ -1,21 +1,71 @@
-const form = document.getElementById('form') as HTMLFormElement;
-const type = document.getElementById('type') as HTMLSelectElement;
-const firstName = document.getElementById('firstName') as HTMLInputElement;
-const lastName = document.getElementById('lastName') as HTMLInputElement;
-const address = document.getElementById('address') as HTMLInputElement;
-const country = document.getElementById('country') as HTMLInputElement;
-const town = document.getElementById('town') as HTMLInputElement;
-const zip = document.getElementById('zip') as HTMLInputElement;
-const product = document.getElementById('product') as HTMLInputElement;
-const price = document.getElementById('price') as HTMLInputElement;
-const quantity = document.getElementById('quantity') as HTMLInputElement;
-const tva = document.getElementById('tva') as HTMLInputElement;
+class FormInput{
+    form: HTMLFormElement;
+    type: HTMLSelectElement;
+    firstName: HTMLInputElement;
+    lastName: HTMLInputElement;
+    address: HTMLInputElement;
+    country: HTMLInputElement;
+    town: HTMLInputElement;
+    zip: HTMLInputElement;
+    product: HTMLInputElement;
+    price: HTMLInputElement;
+    quantity: HTMLInputElement;
+    tva: HTMLInputElement;
 
+    constructor(){
+        this.form = document.getElementById('form') as HTMLFormElement;
+        this.type = document.getElementById('type') as HTMLSelectElement;
+this.firstName = document.getElementById('firstName') as HTMLInputElement;
+this.lastName = document.getElementById('lastName') as HTMLInputElement;
+this.address = document.getElementById('address') as HTMLInputElement;
+this.country = document.getElementById('country') as HTMLInputElement;
+this.town = document.getElementById('town') as HTMLInputElement;
+this.zip = document.getElementById('zip') as HTMLInputElement;
+this.product = document.getElementById('product') as HTMLInputElement;
+this.price = document.getElementById('price') as HTMLInputElement;
+this.quantity = document.getElementById('quantity') as HTMLInputElement;
+this.tva = document.getElementById('tva') as HTMLInputElement;
 
-// submit form
-form.addEventListener("submit", (e: Event) => {
-    e.preventDefault();
+this.submitFormListener()
 
-    console.log(type.value, firstName.value, lastName.value, address.value, country.value, town.value, product.value, zip.value, price.value, quantity.value, tva.value);
-    
-})
+    }
+
+    // Listeners
+    private submitFormListener(): void {
+        this.form.addEventListener('submit', this.handleFormSubmit.bind(this))
+    }
+    private handleFormSubmit(e: Event){
+        e.preventDefault();
+        const inputs =  this.inputData();
+
+        if (Array.isArray(inputs)){
+          const [type, firstName, lastName, address, country, town, zip, product, price, quantity, tva] = inputs;
+          console.log(type, firstName, lastName, address, country, town, zip, product, price, quantity, tva);
+          
+        }
+
+    }
+    private inputData():[string, string, string,string,string,string,number,string,number,number,number] | void {
+        const type = this.type.value;
+        const firstName = this.firstName.value;
+        const lastName = this.lastName.value;
+        const address = this.address.value;
+        const country = this.country.value;
+        const town =  this.town.value;
+        const zip = this.zip.valueAsNumber;
+        const product = this.product.value;
+        const price = this.price.valueAsNumber;
+        const quantity = this.quantity.valueAsNumber;
+        const tva = this.tva.valueAsNumber;
+
+if( zip > 0 && price > 0 && quantity > 0 && tva > 0){
+   return [type, firstName, lastName, address, country, town, zip, product, price, quantity, tva]
+}else{
+    alert("Les valeurs numeriques doivent etre superieur a zero")
+    return;
+}
+
+    }
+}
+
+new FormInput();
