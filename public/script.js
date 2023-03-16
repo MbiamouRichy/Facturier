9b243df52dfa3e4e54784f1886c14947c14487dc
Richy <richymbiamou52@gmail.com>
2023-03-16 19:34:39 +0100

    test du travail

--- public/script.js
+++ public/script.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log('richy');
+const form = document.getElementById('form');
+const type = document.getElementById('type');
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const address = document.getElementById('address');
+const country = document.getElementById('country');
+const town = document.getElementById('town');
+const zip = document.getElementById('zip');
+const product = document.getElementById('product');
+const price = document.getElementById('price');
+const quantity = document.getElementById('quantity');
+const tva = document.getElementById('tva');
+// submit form
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(type.value, firstName.value, lastName.value, address.value, country.value, town.value, product.value, zip.value, price.value, quantity.value, tva.value);
+});
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,21 @@
-console.log('richy')
+const form = document.getElementById('form') as HTMLFormElement;
+const type = document.getElementById('type') as HTMLSelectElement;
+const firstName = document.getElementById('firstName') as HTMLInputElement;
+const lastName = document.getElementById('lastName') as HTMLInputElement;
+const address = document.getElementById('address') as HTMLInputElement;
+const country = document.getElementById('country') as HTMLInputElement;
+const town = document.getElementById('town') as HTMLInputElement;
+const zip = document.getElementById('zip') as HTMLInputElement;
+const product = document.getElementById('product') as HTMLInputElement;
+const price = document.getElementById('price') as HTMLInputElement;
+const quantity = document.getElementById('quantity') as HTMLInputElement;
+const tva = document.getElementById('tva') as HTMLInputElement;
+
+
+// submit form
+form.addEventListener("submit", (e: Event) => {
+    e.preventDefault();
+
+    console.log(type.value, firstName.value, lastName.value, address.value, country.value, town.value, product.value, zip.value, price.value, quantity.value, tva.value);
+    
+})
